6ac01c4fce108c5b0cc6e796e55f250e6d9f4eb0
Evgeniy Golubev <dev.egolubev@gmail.com>
2020-10-20 17:24:03 +0300

    init

---
+++ gatsby-config.js
@@ -0,0 +1,44 @@
+module.exports = {
+  siteMetadata: {
+    title: `Vector Inc`,
+    description: `Strapi & Gatsby site of Vector`,
+    author: `gee@vector-inc.dev`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: "gatsby-source-strapi",
+      options: {
+        apiURL: process.env.DEPLOY_URL
+          ? "https://vector-inc-content-api.herokuapp.com"
+          : "http://localhost:1337",
+        contentTypes: [],
+        queryLimit: 1000,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+}
