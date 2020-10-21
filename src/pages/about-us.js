aa446acd09c90e7d737bdedc840f9639f612630a
ed <makukhined@gmai.com>
2020-10-21 17:54:30 +0300

    feature (pages) add about, blog, expertise, portfolio demo pages

---
+++ src/pages/about-us.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const AboutUsPage = () => (
+    <Layout>
+        <SEO title="AboutUs Page" />
+        <h1>About Us works!</h1>
+    </Layout>
+)
+
+export default AboutUsPage;
---
+++ src/pages/blog.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const BlogPage = () => (
+    <Layout>
+        <SEO title="Blog Page" />
+        <h1>Blog works!</h1>
+    </Layout>
+)
+
+export default BlogPage;
