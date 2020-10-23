2d0f2be1764bf4dd8f46a2d637f4a5b25f5b36b4
Andrew <averingood@gmail.com>
2020-10-23 13:54:29 +0300

    refactor (index, particles, particles-options) move particles options in separate file

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,14 @@
 import React from "react"
+import Particles from "react-tsparticles";
 
 import Layout from "../components/layout"
-import ParticlesCustom from "../components/particles/particles";
 import HomeBlock from "../components/home-block/home-block";
+import { options } from '../components/particles/particles-options';
 
 const IndexPage = () => (
     <Layout>
         <div className="particles-container">
-            <ParticlesCustom />
+            <Particles id="tsparticles" options={options} />
         </div>
 
         <HomeBlock />
