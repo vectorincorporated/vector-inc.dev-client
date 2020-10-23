457d0c11e962749d4700a94ae184dd2e29c697e8
averingood <34921941+averingood@users.noreply.github.com>
2020-10-23 17:14:12 +0300

    Feature/footer (#2)
    * feature (header, layout, fonts, logo, menu-icon, index)
    - create header
    - include VT323 font
    - add logo
    - move header in separate directory
    - replace main items on the welcome block
    
    * feature (home-block, particles, index, package, gatsby-browser)
    - create component for home block
    - add lines animation
    - add particles animation
    
    * refactor (layout, gatsby-browser) move styles to layout
    
    * refactor (index, particles, particles-options) move particles options in separate file
    
    * feature (footer, layout) create footer
    
    * feature (footer) add pink icons on hover
    
    * git commit -m ".gitignore Fixed"
    
    * refactor (footer) use classNames for multiple classes
    
    * Revert "git commit -m ".gitignore Fixed""
    
    This reverts commit cf7fedb7ae3b0371f1443085f7696f9c6f4cdb5c.
    
    * refactor (particles-option) remove excess file
    
    Co-authored-by: ed <makukhined@gmai.com>

---
+++ src/components/footer/footer.js
@@ -0,0 +1,30 @@
+import React from "react"
+
+import mail from '../../images/icons/mail.svg';
+import styles from './footer.module.css';
+
+const classNames = require('classnames');
+
+const Footer = () => (
+    <footer>
+        <div className={styles.wrapper}>
+            <div className={styles.mailWrapper}>
+                <img src={mail} alt="mail"/>
+                <div className={styles.smallDivider}>....</div>
+                <a className={styles.mailLink} href='mailto:vector@mail.com'>vector@mail.com</a>
+            </div>
+
+            <span className={styles.divider}>........................</span>
+
+            <div className={styles.socialsWrapper}>
+                <span>follow us</span>
+                <div className={styles.smallDivider}>....</div>
+                <a href='#' className={classNames(styles.linkedin, styles.icon)} />
+                <a href='#' className={classNames(styles.facebook, styles.icon)} />
+                <a href='#' className={classNames(styles.instagram, styles.icon)} />
+            </div>
+        </div>
+    </footer>
+);
+
+export default Footer
