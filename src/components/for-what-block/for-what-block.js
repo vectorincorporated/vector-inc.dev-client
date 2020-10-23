abd71350efd4f54aaefaef61453ed128010b01c9
Andrew <averingood@gmail.com>
2020-10-23 15:11:46 +0300

    feature (home-block, particles, index, package, gatsby-browser) create for what block

---
+++ src/components/for-what-block/for-what-block.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+import styles from './for-what-block.module.css';
+
+const ForWhatBlock = () => {
+    return (
+        <div>
+            <div className={ styles.title + ' wrap' }>
+                <h2>
+                    For what
+                </h2>
+            </div>
+        </div>
+    )
+};
+
+export default ForWhatBlock
