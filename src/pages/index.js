cae81267ce1823467c71cb4140d3fe85e4fdd587
Andrew <averingood@gmail.com>
2020-10-23 15:15:35 +0300

    refactor (layout, index, particles-options) move particles-options to utils, remove width from main block

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Particles from "react-tsparticles";
 
 import Layout from "../components/layout"
 import HomeBlock from "../components/home-block/home-block";
-import { options } from '../components/particles/particles-options';
+import { options } from '../utils/particles/particles-options';
 import SEO from "../components/seo";
 
 const IndexPage = () => (
