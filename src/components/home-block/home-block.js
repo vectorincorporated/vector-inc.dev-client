db5e5e02246048a15166e919431fb40314870606
Andrew <averingood@gmail.com>
2020-10-23 13:57:50 +0300

    refactor (home-block, index) move SEO to index js

--- src/components/home-block/home-block.js
+++ src/components/home-block/home-block.js
@@ -1,13 +1,10 @@
 import React from "react"
 
-import SEO from "../seo";
 import styles from './home-block.module.css';
 
 const HomeBlock = () => {
     return (
         <div>
-            <SEO title="Home" />
-
             <div className={ styles.title + ' wrap' }>
                 <h1 className='glitch' data-text="WE USE THE BEST TECHNOLOGIES TO ENHANCE YOUR PRODUCT">
                     We use the best technologies <br/>to enhance your product
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,9 +4,12 @@ import Particles from "react-tsparticles";
 import Layout from "../components/layout"
 import HomeBlock from "../components/home-block/home-block";
 import { options } from '../components/particles/particles-options';
+import SEO from "../components/seo";
 
 const IndexPage = () => (
     <Layout>
+        <SEO title="Home" />
+
         <div className="particles-container">
             <Particles id="tsparticles" options={options} />
         </div>
