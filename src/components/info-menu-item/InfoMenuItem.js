75e989680e49fb2fe35ca8d4088105bed61f458a
ed <makukhined@gmai.com>
2020-10-26 18:32:09 +0200

    refactor (InfoMenuItem) format code

--- src/components/info-menu-item/InfoMenuItem.js
+++ src/components/info-menu-item/InfoMenuItem.js
@@ -1,10 +1,13 @@
 import React from "react";
 
 const InfoMenuItem = ({ toggleInfoBlock, styles, text }) => (
-    <div className={styles.infoItem} onMouseOver={() => toggleInfoBlock(true)} onMouseLeave={() => toggleInfoBlock(false)}>
+    <div className={styles.infoItem}
+         onMouseOver={() => toggleInfoBlock(true)}
+         onMouseLeave={() => toggleInfoBlock(false)}>
+
         <span>{ text }</span>
-        <div className={styles.line}>
-        </div>
+
+        <div className={styles.line} />
     </div>
 )
 
