56a13eb5120d6cba518be504c89ff16de5283701
Andrew <averingood@gmail.com>
2020-10-26 15:52:23 +0200

    feature (for-what-block, layout, ubuntu mono) add static elements on block, include "ubuntu mono" font, remove excess component

--- src/components/for-what-block/for-what-block.js
+++ src/components/for-what-block/for-what-block.js
@@ -1,17 +1,83 @@
-import React from "react"
+import React, {useState} from "react"
 
 import styles from './for-what-block.module.css';
 
+const classNames = require('classnames');
+
+
 const ForWhatBlock = () => {
+    const [isInfoBlock, toggleInfoBlock] = useState(false);
+
+    const showInfoBlock = () => {
+        toggleInfoBlock(true);
+    };
+    
+    const hideInfoBlock = () => {
+        toggleInfoBlock(false);
+    };
+
     return (
-        <div>
-            <div className={ styles.title + ' wrap' }>
-                <h2>
+        <div className={ styles.forWhat }>
+            <div className={styles.header}>
+                <h2 className={ styles.title }>
                     For what
                 </h2>
+                <span className='accent-text'>
+                    we use the best technologiies to enhance your product
+                </span>
+            </div>
+
+            <div className={ styles.info }>
+                <div className={classNames( 'accent-text', styles.infoMenu )}>
+                    <div className={styles.infoItem} onMouseOver={showInfoBlock} onMouseLeave={hideInfoBlock}>
+                        <span>Enhance your internal communication</span>
+                        <div className={styles.line}>
+                        </div>
+                    </div>
+
+                    <div className={styles.infoItem} onMouseOver={showInfoBlock} onMouseLeave={hideInfoBlock}>
+                        <span>Boost your marketing effort</span>
+                        <div className={styles.line}>
+                        </div>
+                    </div>
+
+                    <div className={styles.infoItem} onMouseOver={showInfoBlock} onMouseLeave={hideInfoBlock}>
+                        <span>Automate your Business processes</span>
+                        <div className={styles.line}>
+                        </div>
+                    </div>
+
+                    <div className={styles.infoItem} onMouseOver={showInfoBlock} onMouseLeave={hideInfoBlock}>
+                        <span>Increase your productivity</span>
+                        <div className={styles.line}>
+                        </div>
+                    </div>
+
+                    <div className={styles.infoItem} onMouseOver={showInfoBlock} onMouseLeave={hideInfoBlock}>
+                        <span>Set up work remotely</span>
+                        <div className={styles.line}>
+                        </div>
+                    </div>
+                </div>
+
+                <div className={ isInfoBlock ? styles.infoBlock : styles.infoBlockHidden }>
+                    <span className='accent-text'>
+                        Enhance your internal communication
+                    </span>
+                    <span className={styles.divider}>
+                        ••••••••••••••••••••••••••••••••••••••
+                    </span>
+                    <span className='regular-text'>
+                        Take the time to explain the company values and mission,
+                        whether as part of corporate training or as a regular reminder to employees.
+                        When employees are unified in their understanding of the company goals,
+                        they will be able to communicate with one another more effectively in order to reach them.
+                    </span>
+                </div>
+
             </div>
         </div>
     )
 };
 
-export default ForWhatBlock
+export default ForWhatBlock;
