943ea86648d8dd198bd089cad7c722ede1c119ad
ed <makukhined@gmai.com>
2020-10-27 18:38:47 +0200

    feature (QuestionsFormBlock) add question form block

---
+++ src/components/question-form-block/QuestionsFormBlock.js
@@ -0,0 +1,44 @@
+import React, {} from "react";
+import classNames from "classnames";
+
+import styles from './QuestionsFormBlock.module.css';
+
+const QuestionsFormBlock = () => {
+    return (
+        <div className={ styles.questions }>
+            <div className={classNames( 'accent-text', styles.main )}>
+                <div className={ styles.header }>
+                    <h2 className={ styles.title }>
+                        HAVE SOME QUESTIONS?
+                    </h2>
+
+                    <span className='accent-text'>
+                        We’re ready to give you professional advice <br/> and a free cost estimate.
+                    </span>
+                </div>
+
+                <div className={styles.form}>
+                    <input className={styles.formField}
+                           type='text'
+                           placeholder='YOUR NAME *' />
+
+                    <input className={styles.formField}
+                           type='text'
+                           placeholder='YOUR E-MAIL *' />
+
+                    <textarea className={styles.formField}
+                              placeholder='LEAVE YOUR MESSAGE HERE... (optional)'/>
+
+                    <button className={styles.btn}>oxxxymiron</button>
+                </div>
+
+            </div>
+
+            <div className={styles.animation}>
+
+            </div>
+        </div>
+    )
+};
+
+export default QuestionsFormBlock;
