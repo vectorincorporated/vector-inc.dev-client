5d535cc4ff84f8734c7ffde9fa049ce0033e185d
averingood <34921941+averingood@users.noreply.github.com>
2020-10-29 17:40:28 +0200

    Feature/expertise (#10)
    * feature (expertise) add expertise block with content
    
    * feature (SphereAnimation, ExpertiseBlock) add component for sphere animation on expertise page
    
    * refactor (ExpertiseItem) move component directory
    
    * refactor (expertise) create another expertise page
    
    * fix (ExpertiseBlock, ExpertiseItem) fix right side style
    
    * refactor (SphereAnimation) use loop for animation elements
    
    * refactor (ExpertiseBlock, ExpertiseSubItemList) rename component
    
    * refactor (ExpertiseBlock, ExpertiseSubItemList, ExpertiseSubItem) decompose to more components
    
    * refactor (ExpertiseBlock, ExpertiseSubItemList) move position styles outside

---
+++ src/components/sphere-animation/SphereAnimation.js
@@ -0,0 +1,22 @@
+import React from "react";
+import classNames from 'classnames';
+
+import styles from './SphereAnimation.module.css';
+
+const circles = [1, 2, 3, 4, 5, 6];
+
+const SphereAnimation = () => {
+    return (
+        <div className={styles.view}>
+            <div className={classNames(styles.plane, styles.main)}>
+                {
+                    circles.map((i) => {
+                        return <div key={i} className={styles.circle} />
+                    })
+                }
+            </div>
+        </div>
+    )
+};
+
+export default SphereAnimation;
--- src/pages/expertise.js
+++ src/pages/expertise.js
@@ -2,12 +2,15 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import ExpertiseBlock from "../components/expertise/ExpertiseBlock";
 
 const ExpertisePage = () => (
     <Layout>
         <SEO title="Expertise Page" />
-        <h1>Expertise works!</h1>
+        <div>
+            <ExpertiseBlock />
+        </div>
     </Layout>
-)
+);
 
 export default ExpertisePage;
