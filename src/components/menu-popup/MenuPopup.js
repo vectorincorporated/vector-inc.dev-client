673e60dd8a9f5eb0b1883f6f0a3efad3588f8c73
Andrew <averingood@gmail.com>
2020-10-30 13:29:56 +0200

    feature (menu) create menu popup

---
+++ src/components/menu-popup/MenuPopup.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { Link } from "gatsby";
+
+import styles from './MenuPopup.module.css';
+
+const links = [
+    {
+        url: '/services',
+        title: 'services'
+    },
+    {
+        url: '/expertise',
+        title: 'expertise'
+    },
+    {
+        url: '/portfolio',
+        title: 'portfolio'
+    },
+    {
+        url: '/about-us',
+        title: 'about us'
+    },
+    {
+        url: '/contact',
+        title: 'contact'
+    },
+    {
+        url: '/blog',
+        title: 'blog'
+    }
+];
+
+const MenuPopup = ({ close }) => (
+    <div className={styles.wrapper}>
+        <div className={styles.header}>
+            <span className={styles.closeBtn} onClick={close}>X</span>
+        </div>
+
+        <div className={styles.links}>
+            {
+                links.map((link) => {
+                    return <Link to={link.url} key={link.url} className={styles.link}>
+                        { link.title }
+                    </Link>
+                })
+            }
+        </div>
+
+    </div>
+);
+
+export default MenuPopup;
--- src/pages/expertise.js
+++ src/pages/expertise.js
@@ -2,13 +2,13 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import ExpertiseBlock from "../components/expertise/ExpertiseBlock";
+// import ExpertiseBlock from "../components/expertise/ExpertiseBlock";
 
 const ExpertisePage = () => (
     <Layout>
         <SEO title="Expertise Page" />
         <div>
-            <ExpertiseBlock />
+            {/*<ExpertiseBlock />*/}
         </div>
     </Layout>
 );
