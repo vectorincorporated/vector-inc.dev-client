bb89c8f28b88828c677f636d0db1a94ed11dc014
Andrew <makukhined@gmai.com>
2020-11-03 17:45:09 +0200

    feature (about-us) add question block

--- src/pages/about-us.js
+++ src/pages/about-us.js
@@ -4,13 +4,19 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import AboutUsBlock from "../components/about-us/AboutUsBlock";
 import OurTeamBlock from "../components/our-team/OurTeamBlock";
+import QuestionsFormBlock from "../components/question-form-block/QuestionsFormBlock";
 
 const AboutUsPage = () => (
     <Layout>
         <SEO title="AboutUs Page" />
         <div>
             <AboutUsBlock />
+        </div>
+        <div>
             <OurTeamBlock />
+        </div>
+        <div>
+            <QuestionsFormBlock />
         </div>
     </Layout>
 );
