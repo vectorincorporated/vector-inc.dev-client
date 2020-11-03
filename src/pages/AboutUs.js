949983661489dc2712b3d380f35b95884c8992fd
Vector Inc <66318526+vectorincorporated@users.noreply.github.com>
2020-11-03 12:45:35 +0200

    Revert "Feature/about us (#13)" (#14)
    This reverts commit 5cb73ea99650264d103271eac70b76cfc517de0e.

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -2,15 +2,12 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import AboutUsBlock from "../components/about-us/AboutUsBlock";
 
 const AboutUsPage = () => (
     <Layout>
         <SEO title="AboutUs Page" />
-        <div>
-            <AboutUsBlock />
-        </div>
+        <h1>About Us works!</h1>
     </Layout>
-);
+)
 
 export default AboutUsPage;
