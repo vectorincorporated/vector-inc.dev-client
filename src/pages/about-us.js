ef867f0ae7b7d6fa0755cbe759fb62c47897b21d
Andrew <makukhined@gmai.com>
2020-11-03 16:43:36 +0200

    feature (OurTeamBlock, MemberTeam, about-us) add photo of members

--- src/pages/about-us.js
+++ src/pages/about-us.js
@@ -3,12 +3,14 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import AboutUsBlock from "../components/about-us/AboutUsBlock";
+import OurTeamBlock from "../components/our-team/OurTeamBlock";
 
 const AboutUsPage = () => (
     <Layout>
         <SEO title="AboutUs Page" />
         <div>
             <AboutUsBlock />
+            <OurTeamBlock />
         </div>
     </Layout>
 );
