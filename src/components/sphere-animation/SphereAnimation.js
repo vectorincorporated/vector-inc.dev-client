185ff939b33f602fa2cc828426a71ba58de52a62
Andrew <averingood@gmail.com>
2020-11-05 12:27:20 +0200

    feature (SphereAnimation) add animation settings

--- src/components/sphere-animation/SphereAnimation.js
+++ src/components/sphere-animation/SphereAnimation.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import classNames from 'classnames';
 
 import stylePinks from './SphereAnimationPink.module.css';
@@ -8,10 +8,14 @@ const circles = [1, 2, 3, 4, 5, 6];
 
 const SphereAnimation = ({ color }) => {
     const styles = color === 'pink' ? stylePinks : styleWhite;
+    const [animation, setAnimation] = useState(false);
 
     return (
         <div className={styles.view}>
-            <div className={classNames(styles.plane, styles.main)}>
+
+            <div className={classNames(styles.plane, styles.main, {
+                'change-plane-size': animation
+            })}>
                 {
                     circles.map((i) => {
                         return <div key={i} className={styles.circle} />
