712e22df9fc5a70414bd8ee2aef3bdd65d121ea4
Andrew <averingood@gmail.com>
2020-11-05 20:38:54 +0200

    feature (ExpertiseBlock, SphereAnumation) make sphere smaller

--- src/components/sphere-animation/SphereAnimation.js
+++ src/components/sphere-animation/SphereAnimation.js
@@ -3,22 +3,24 @@ import classNames from 'classnames';
 
 import stylePinks from './SphereAnimationPink.module.css';
 import styleWhite from './SphereAnimationWhite.module.css';
+import "./SphereSizeAnimation.css";
 
 const circles = [1, 2, 3, 4, 5, 6];
 
-const SphereAnimation = ({ color }) => {
+const SphereAnimation = ({ color, smallerSphere }) => {
     const styles = color === 'pink' ? stylePinks : styleWhite;
-    const [animation, setAnimation] = useState(false);
 
     return (
         <div className={styles.view}>
 
             <div className={classNames(styles.plane, styles.main, {
-                'change-plane-size': animation
+                'change-plane-size': smallerSphere
             })}>
                 {
                     circles.map((i) => {
-                        return <div key={i} className={styles.circle} />
+                        return <div key={i} className={classNames(styles.circle, {
+                            'change-circle-size': smallerSphere
+                        })} />
                     })
                 }
             </div>
