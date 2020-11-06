90446a3efd62d7f1c8f4938a5912d44504034d97
Andrew <averingood@gmail.com>
2020-11-06 17:32:17 +0200

    feature (OurAddress) demo map

--- src/components/our-address/OurAddress.js
+++ src/components/our-address/OurAddress.js
@@ -1,7 +1,28 @@
 import React, {} from "react";
-import { GoogleMap, Marker } from "react-google-maps"
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
 
 import styles from './OurAddress.module.css';
+import { compose, withProps } from "recompose";
+import settings from "./map-settings";
+
+const MyMapComponent = compose(
+    withProps({
+        googleMapURL: "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places",
+        loadingElement: <div style={{ height: `100%` }} />,
+        containerElement: <div style={{ height: `400px` }} />,
+        mapElement: <div style={{ height: `100%` }} />,
+    }),
+    withScriptjs,
+    withGoogleMap
+)(props =>
+    <GoogleMap
+        defaultZoom={8}
+        defaultCenter={{ lat: 49.988358, lng: 36.232845 }}
+    >
+        {props.isMarkerShown && <Marker position={{ lat: 49.988358, lng: 36.232845 }} />}
+    </GoogleMap>
+);
+
 
 const info = {
     title: 'Our Address',
@@ -12,11 +33,12 @@ const OurAddressBlock = () => {
     return (
         <div className={ styles.ourAddress }>
             <div className={ styles.info }>
-                <div className={styles.infoTitle}>{ info.title }</div>
+                <div className='accent-text'>{ info.title }</div>
                 <div className={styles.divider}>••••••••••••••••••••••••••••••••••••••</div>
 
                 <div className={styles.map}>
-                    <GoogleMap />
+                    <MyMapComponent isMarkerShown />
+                    {/*<GoogleMap />*/}
                 </div>
             </div>
         </div>
