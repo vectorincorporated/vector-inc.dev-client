42d9749f7cba42dc36a1ac099e9c12d75276e45b
Andrew <averingood@gmail.com>
2020-11-09 14:44:21 +0200

    refactor (ContactsBlock) remove unused import

--- src/components/contacts/ContactsBlock.js
+++ src/components/contacts/ContactsBlock.js
@@ -1,5 +1,4 @@
 import React, {} from "react";
-import classNames from "classnames";
 
 import styles from './ContactsBlock.module.css';
 import facebook from '../../images/icons/contacts/facebook.svg';
