941f1e75f3439bd932e8968bdf80c0f0274bc24c
Andrew <averingood@gmail.com>
2020-11-09 15:13:06 +0200

    refactor (Global) remove unused import, fix flex styles

--- src/components/project-problems/ProjectDetailsBlock.js
+++ src/components/project-problems/ProjectDetailsBlock.js
@@ -1,5 +1,4 @@
-import React, {} from "react";
-import classNames from "classnames";
+import React from "react";
 
 import styles from './ProjectDetailsBlock.module.css';
 import InfoBlock from "../info-block/InfoBlock";
