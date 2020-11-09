2e4a22b15c42861319607eafb32ce1de72b0ed93
Andrew <averingood@gmail.com>
2020-11-09 14:10:19 +0200

    feature (ThanksPopup, FormBtn) add thanks popup after form sending

--- src/components/form/form-btn/FormBtn.js
+++ src/components/form/form-btn/FormBtn.js
@@ -1,10 +1,16 @@
 import React from "react";
 
 import styles from "./FormBtn.module.css";
+import ThanksPopup from "../../thanks-popup/ThanksPopup";
+import Popup from "reactjs-popup";
 
 const FormBtn = ({ options }) => {
     return (
-        <button className={options?.isBtnPink ? styles.btnPink : styles.btn}>send</button>
+        <Popup trigger={
+            <button className={options?.isBtnPink ? styles.btnPink : styles.btn}>send</button>
+        } modal>
+            { close => <ThanksPopup close={close}/> }
+        </Popup>
     )
 }
 
