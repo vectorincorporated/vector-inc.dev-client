1312aca3692ed6075b249edbb160c89c3798eb93
Andrew <averingood@gmail.com>
2020-11-10 13:31:19 +0200

    refactor (Map) format code

--- src/components/our-address/map/Map.js
+++ src/components/our-address/map/Map.js
@@ -3,10 +3,12 @@ import React from "react";
 
 const mapStyles = require("./map-settings.json");
 
+const key = 'AIzaSyC4R6AN7SmujjPUIGKdyao2Kqitzr1kiRg';
+
 const Map = compose(
     withProps({
         // TODO: Create company account and get google maps APIKEY
-        googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyC4R6AN7SmujjPUIGKdyao2Kqitzr1kiRg&v=3.exp&libraries=geometry,drawing,places",
+        googleMapURL: 'https://maps.googleapis.com/maps/api/js?key=' + key + 'v=3.exp&libraries=geometry,drawing,places',
         loadingElement: <div style={{ height: `100%` }} />,
         containerElement: <div style={{ height: `400px` }} />,
         mapElement: <div style={{ height: `100%` }} />,
