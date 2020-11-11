61d62d891207d7fbce55eb830f813248f3babc3e
Andrew <averingood@gmail.com>
2020-11-11 15:46:25 +0200

    fix (DottedItem, Tags) dotted item hover color, fix warnings

--- src/components/dotted-item/DottedItem.js
+++ src/components/dotted-item/DottedItem.js
@@ -5,7 +5,7 @@ import styles from "./DottedItem.module.css";
 
 const DottedItem = ({ item, isPink }) => {
     return (
-        <div className={ isPink ? styles.itemPink : styles.item }>
+        <div className={classNames(styles.item, isPink ? styles.itemPink : '')}>
             <div className={ isPink ? styles.circleLeftPink: styles.circleLeft }/>
             <div className={ classNames(styles.itemText, 'regular-text') }>
                 { item }
