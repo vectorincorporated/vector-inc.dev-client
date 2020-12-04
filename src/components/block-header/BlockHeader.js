d1f22d7ebdab3d88990055dbfd51f3680f83380c
Andrew <averingood@gmail.com>
2020-12-04 11:50:01 +0200

    refactor (Form, Header, HomeBlock, ContactFormPopup< BlockHeader) change text of popup form

--- src/components/block-header/BlockHeader.js
+++ src/components/block-header/BlockHeader.js
@@ -1,10 +1,11 @@
 import React from "react";
+import classNames from 'classnames';
 
 import styles from './BlockHeader.module.css';
 
-const BlockHeader = ({ header }) => {
+const BlockHeader = ({ header, centralized }) => {
     return (
-        <div className={ styles.wrapper }>
+        <div className={classNames(styles.wrapper, centralized ? styles.centralized : '')}>
             <h2 className={ styles.title }>
                 { header.title }
             </h2>
--- src/components/contact-form-popup/ContactFormPopup.js
+++ src/components/contact-form-popup/ContactFormPopup.js
@@ -4,32 +4,38 @@ import styles from './ContactFormPopup.module.css';
 import Form from "../form/Form";
 import BlockHeader from "../block-header/BlockHeader";
 
-const header = {
-    title: 'LEAVE YOUR CONTACTS',
-    description: 'WE WILL CONTACT WITH YOU AS SOON AS POSIBLE'
-};
+const ContactFormPopup = ({ close, headerOptions, formOptions }) => {
+    const formProps = formOptions || {
+        isBtnPink: true,
+        isPhone: true,
+        placeholders: {
+            name: '',
+            email: '',
+            phone: '',
+            message: ''
+        }
+    };
 
-const formOptions = {
-    isBtnPink: true,
-    isPhone: true
-};
+    const header = headerOptions || {
+        title: 'LEAVE YOUR CONTACTS',
+        description: 'WE WILL CONTACT WITH YOU AS SOON AS POSIBLE'
+    };
 
-const ContactFormPopup = ({ close }) => (
-    <div className={styles.wrapper}>
+    return <div className={styles.wrapper}>
         <div className={styles.closeBtnWrapper}>
             <span className={styles.closeBtn} onClick={close}>X</span>
         </div>
 
         <div className={styles.content}>
-            <div className={ styles.header }>
-                <BlockHeader header={ header } />
+            <div className={styles.header}>
+                <BlockHeader header={header} centralized={true} />
             </div>
 
             <div className={styles.form}>
-                <Form options={formOptions} close={close} />
+                <Form options={formProps} close={close}/>
             </div>
         </div>
     </div>
-);
+};
 
 export default ContactFormPopup;
