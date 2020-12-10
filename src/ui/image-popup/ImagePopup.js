1a6149a3d71732da2279eb485cb341c30f84d443
Andrew <averingood@gmail.com>
2020-12-10 19:48:14 +0200

    feature (slick-slider, CustomSlider, ImagePopup, ProjectDetailsBlock) create custom slider for portfolio details page

--- src/ui/image-popup/ImagePopup.js
+++ src/ui/image-popup/ImagePopup.js
@@ -1,13 +1,38 @@
 import React from "react";
 
 import styles from './ImagePopup.module.css';
+import Slider from "react-slick/lib";
 
-const ImagePopup = ({ close, image }) => (
-    <div className={styles.wrapper} onClick={close}>
-        <div className={styles.image}>
-            { image }
+const ImagePopup = ({ close, imageList, imageIndex }) => {
+    const settings = {
+        infinite: true,
+        speed: 500,
+        swipe: false,
+        slidesToShow: 1,
+        initialSlide: imageIndex,
+        slidesToScroll: 1
+    };
+
+    return (
+        <div className={styles.wrapper}>
+            {/*TODO: create separate component for close btn*/}
+            <div className={styles.closeBtnWrapper}>
+                <span className={styles.closeBtn} onClick={close}>X</span>
+            </div>
+
+            <div className={styles.sliderWrapperChild}>
+                <Slider {...settings}>
+                    {
+                        imageList?.map((img) => {
+                            return <div key={imageIndex} className={styles.sliderItem}>
+                                <img src={img} alt=''/>
+                            </div>
+                        })
+                    }
+                </Slider>
+            </div>
         </div>
-    </div>
-);
+    )
+};
 
 export default ImagePopup;
