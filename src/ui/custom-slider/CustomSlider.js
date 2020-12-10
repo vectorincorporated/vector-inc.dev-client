243a6577cd77591020a138da867843d66bd80bec
Andrew <averingood@gmail.com>
2020-12-10 19:48:14 +0200

    feature (slick-slider, CustomSlider, ImagePopup, ProjectDetailsBlock) create custom slider for portfolio details page

--- src/ui/custom-slider/CustomSlider.js
+++ src/ui/custom-slider/CustomSlider.js
@@ -3,18 +3,28 @@ import Slider from "react-slick";
 
 import defaultSettings from './default-settings';
 import styles from './CustomSlider.module.css';
+import Popup from "reactjs-popup";
+import ImagePopup from "../image-popup/ImagePopup";
 
 const CustomSlider = ({ imgList, settings = defaultSettings }) => {
     return (
-        <Slider { ...settings }>
-            {
-                imgList?.map((img, i) => {
-                    return <div key={i} className={styles.sliderItem}>
-                        <img src={img} alt='' />
-                    </div>
-                })
-            }
-        </Slider>
+        <div className={styles.sliderWrapper}>
+            <Slider { ...settings }>
+                {
+                    imgList?.map((img, i) => {
+                        return <div key={i} className={styles.sliderItem}>
+                            <Popup modal trigger={
+                                <img src={img} alt=''/>
+                            }>
+                                { close => <ImagePopup close={close}
+                                                       imageIndex={i}
+                                                       imageList={imgList} /> }
+                            </Popup>
+                        </div>
+                    })
+                }
+            </Slider>
+        </div>
     );
 };
 
--- src/ui/custom-slider/default-settings.js
+++ src/ui/custom-slider/default-settings.js
@@ -3,26 +3,34 @@ const defaultSettings = {
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 1,
+    swipe: false,
+    variableWidth: true,
     responsive: [
         {
             breakpoint: 1024,
             settings: {
                 slidesToShow: 3,
-                slidesToScroll: 3,
+                slidesToScroll: 1,
+                swipe: false,
+                variableWidth: true,
                 infinite: true
             }
         },
         {
             breakpoint: 720,
             settings: {
+                swipe: false,
+                variableWidth: true,
                 slidesToShow: 2,
-                slidesToScroll: 2,
+                slidesToScroll: 1,
                 initialSlide: 2
             }
         },
         {
             breakpoint: 480,
             settings: {
+                variableWidth: true,
+                swipe: false,
                 slidesToShow: 1,
                 slidesToScroll: 1
             }
