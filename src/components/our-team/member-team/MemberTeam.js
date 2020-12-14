3b2243f314d3a047fac8478c4c651c7938475a9e
Andrew <averingood@gmail.com>
2020-12-14 17:37:38 +0200

    refactor (MemberTeam, VhsImage) remove extra component

--- src/components/our-team/member-team/MemberTeam.js
+++ src/components/our-team/member-team/MemberTeam.js
@@ -2,14 +2,13 @@ import React from "react";
 import classNames from 'classnames';
 
 import styles from './MemberTeam.module.css';
-import VhsImage from "../../../ui/vhs-image/VhsImage";
 
 const MemberTeam = ({ member }) => {
     return (
         <div className={styles.member}>
 
             <div className={styles.memberPhoto}>
-                <VhsImage image={member.photo}/>
+                <img src={member.photo} alt="" className='js-vhs-filter'/>
             </div>
 
             <span className={classNames('accent-text', styles.name)}>{ member.name }</span>
