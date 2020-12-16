7b942e9f97ee9a9a389f0ca1d48c32b308c2c1a5
Andrew <averingood@gmail.com>
2020-12-16 18:41:33 +0200

    feature (Header, HeaderMobile) - create component for mobile version - install react-hook/window-size - add logic for mobile template

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,37 +2,48 @@ import React from "react"
 import Popup from 'reactjs-popup';
 import 'reactjs-popup/dist/index.css';
 import {Link} from "gatsby";
+import {useWindowWidth} from "@react-hook/window-size";
 
 import logo from '../../images/logo.svg';
 import menu from '../../images/menu-icon.svg';
 import headerStyles from './Header.module.css';
 import MenuPopup from "../../ui/menu-popup/MenuPopup";
 import ContactFormPopup from "../../ui/contact-form-popup/ContactFormPopup";
+import HeaderMobile from "./HeaderMobile";
 
 const headerOptions = {
     title: 'LEAVE YOUR CONTACTS',
     description: 'WE WILL CONTACT WITH YOU AS SOON AS POSSIBLE'
 };
 
-const Header = () => (
-    <header>
-        <div className={headerStyles.wrapper}>
-            <div className={headerStyles.menu}>
-                <Popup trigger={ <img src={menu} alt="menu"/> } modal>
-                    { close => <MenuPopup close={close}/> }
-                </Popup>
-            </div>
 
-            <Link to={'/'}>
-                <img src={logo} className={headerStyles.logo} alt="Vector"/>
-            </Link>
+const Header = () => {
+    const windowWidth = useWindowWidth();
+    return (
+        <>
+            { windowWidth > 768 ?
+                <header>
+                    <div className={headerStyles.wrapper}>
+                        <div className={headerStyles.menu}>
+                            <Popup trigger={ <img src={menu} alt="menu"/> } modal>
+                                { close => <MenuPopup close={close}/> }
+                            </Popup>
+                        </div>
 
-            <Popup trigger={ <button className={headerStyles.contactUsBtn}>contact us</button> } modal>
-                { close => <ContactFormPopup close={close} headerOptions={headerOptions} /> }
-            </Popup>
+                        <Link to={'/'}>
+                            <img src={logo} className={headerStyles.logo} alt="Vector"/>
+                        </Link>
 
-        </div>
-    </header>
-);
+                        <Popup trigger={ <button className={headerStyles.contactUsBtn}>contact us</button> } modal>
+                            { close => <ContactFormPopup close={close} headerOptions={headerOptions} /> }
+                        </Popup>
+                    </div>
+                </header>
+                :
+                <HeaderMobile />
+            }
+        </>
+    );
+};
 
 export default Header
---
+++ src/components/header/HeaderMobile.js
@@ -0,0 +1,27 @@
+import React from "react"
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
+import {Link} from "gatsby";
+
+import logo from '../../images/logo.svg';
+import menu from '../../images/menu-icon-mobile.svg';
+import styles from './HeaderMobile.module.css';
+import MenuPopup from "../../ui/menu-popup/MenuPopup";
+
+const HeaderMobile = () => {
+    return (
+        <header>
+            <div className={styles.wrapper}>
+                <Link to={'/'}>
+                    <img src={logo} className={styles.logo} alt="Vector"/>
+                </Link>
+
+                <Popup trigger={ <img src={menu} alt="menu"/> } modal>
+                    { close => <MenuPopup close={close}/> }
+                </Popup>
+            </div>
+        </header>
+    );
+};
+
+export default HeaderMobile;
