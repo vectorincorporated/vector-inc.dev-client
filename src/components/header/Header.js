fb4af2673e31672115821289d6b80f4ac0c5a33b
Andrew <averingood@gmail.com>
2020-12-18 11:11:37 +0200

    refactor (HeaderMobile, HeaderDesktop, Header) create component for desktop header, restructure template

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,49 +1,23 @@
-import React from "react"
-import Popup from 'reactjs-popup';
+import React from "react";
 import 'reactjs-popup/dist/index.css';
-import {Link} from "gatsby";
 import {useWindowWidth} from "@react-hook/window-size";
 
-import logo from '../../images/logo.svg';
-import menu from '../../images/menu-icon.svg';
-import headerStyles from './Header.module.css';
-import MenuPopup from "../menu-popup/MenuPopup";
-import ContactFormPopup from "../contact-form-popup/ContactFormPopup";
 import HeaderMobile from "./HeaderMobile";
-
-const headerOptions = {
-    title: 'LEAVE YOUR CONTACTS',
-    description: 'WE WILL CONTACT WITH YOU AS SOON AS POSSIBLE'
-};
-
+import HeaderDesktop from "./HeaderDesktop";
 
 const Header = () => {
     const windowWidth = useWindowWidth();
     return (
-        <>
-            <header>
-                { windowWidth > 768 ?
-                    <div className={headerStyles.wrapper}>
-                        <div className={headerStyles.menu}>
-                            <Popup trigger={ <img src={menu} alt="menu"/> } modal>
-                                { close => <MenuPopup close={close}/> }
-                            </Popup>
-                        </div>
-
-                        <Link to={'/'}>
-                            <img src={logo} className={headerStyles.logo} alt="Vector"/>
-                        </Link>
-
-                        <Popup trigger={ <button className={headerStyles.contactUsBtn}>contact us</button> } modal>
-                            { close => <ContactFormPopup close={close} headerOptions={headerOptions} /> }
-                        </Popup>
-                    </div>
-                    :
-                    <HeaderMobile />
+        <div>
+                {
+                    windowWidth > 768 ?
+                        <HeaderDesktop />
+                        :
+                        <HeaderMobile />
                 }
-            </header>
-        </>
+        </div>
     );
 };
 
-export default Header
+export default Header;
+
