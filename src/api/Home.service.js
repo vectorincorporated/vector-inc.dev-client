6e73c39a233edc18113c582f3c0b9b73bde37791
Andrew <averingood@gmail.com>
2020-12-29 11:32:11 +0200

    feature (Home.service, ForWhatBlock, InfoBlock, ServicesHomeBlock, index) text integration on home page

---
+++ src/api/Home.service.js
@@ -0,0 +1,10 @@
+import environment from '../environments/environment';
+
+const HomeService = {
+    getHome: () => {
+        return fetch(environment.apiUrl + '/home-page')
+            .then(res => res.json());
+    }
+};
+
+export default HomeService;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, {useEffect, useState} from "react"
 import Particles from "react-tsparticles";
 import Helmet from "react-helmet";
 
@@ -11,39 +11,56 @@ import HowWeWorkBlock from "../components/how-we-work/HowWeWorkBlock";
 import ServicesHomeBlock from "../components/services-home-block/ServicesHomeBlock";
 import QuestionsFormBlock from "../components/question-form-block/QuestionsFormBlock";
 import OurClientsBlock from "../components/our-clients-block/OurClientsBlock";
+import HomeService from "../api/Home.service";
 
-const IndexPage = () => (
-    // TODO: clear and make redirect to Home component
-    <Layout>
-        <SEO title="Home" />
+const IndexPage = () => {
+    const [homeData, setHomeData] = useState(null);
 
-        <div className="particles-container">
-            <Particles id="tsparticles" options={options} />
-        </div>
+    useEffect(() => {
+        HomeService.getHome()
+            .then((result) => {
+                setHomeData(result);
+            })
+    }, []);
 
-        <div className='full-height-view container'>
-            <HomeBlock />
-        </div>
-        <div className='full-height-view container'>
-            <ForWhatBlock />
-        </div>
-        <div className='full-height-view container'>
-            <ServicesHomeBlock />
-        </div>
-        <div className='container'>
-            <HowWeWorkBlock />
-        </div>
-        <div className='container'>
-            <OurClientsBlock />
-        </div>
-        <div className='container'>
-            <QuestionsFormBlock />
-        </div>
+    return (
+        // TODO: clear and make redirect to Home component
+        <Layout>
+            <SEO title="Home" />
 
-        <Helmet>
-            <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1" />
-        </Helmet>
-    </Layout>
-);
+            <div className="particles-container">
+                <Particles id="tsparticles" options={options} />
+            </div>
+
+            <div className='full-height-view container'>
+                <HomeBlock />
+            </div>
+
+            <div className='full-height-view container'>
+                <ForWhatBlock forWhat={homeData?.forWhatBlock} />
+            </div>
+
+            <div className='full-height-view container'>
+                <ServicesHomeBlock services={homeData?.servicesBlock} />
+            </div>
+
+            <div className='container'>
+                <HowWeWorkBlock />
+            </div>
+
+            <div className='container'>
+                <OurClientsBlock />
+            </div>
+
+            <div className='container'>
+                <QuestionsFormBlock />
+            </div>
+
+            <Helmet>
+                <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1" />
+            </Helmet>
+        </Layout>
+    );
+};
 
 export default IndexPage
