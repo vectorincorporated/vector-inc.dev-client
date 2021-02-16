91c84c3cc7b5fe2228ba531abb902a123a824d5e
Maryna Makukhina <maryna.makukhina@gmail.com>
2021-02-16 17:03:58 +0200

    update prettierrc configuration: allow semi

--- src/components/service-item/ServiceItem.js
+++ src/components/service-item/ServiceItem.js
@@ -1,14 +1,18 @@
 import React from "react";
-import classNames from 'classnames';
+import classNames from "classnames";
 
-import styles from './ServiceItem.module.css';
+import styles from "./ServiceItem.module.css";
 
 const ServiceItem = ({ toggleInfoBlock, item, activeItem }) => (
-    <div className={classNames(styles.infoItem, activeItem === item ? styles.active : '') }
-         role = "button"
-         onClick={(e) => toggleInfoBlock(e, item)}>
-
-        <span>{ item.title }</span>
+    <div
+        className={classNames(
+            styles.infoItem,
+            activeItem === item ? styles.active : ""
+        )}
+        role="button"
+        onClick={e => toggleInfoBlock(e, item)}
+    >
+        <span>{item.title}</span>
     </div>
 );
 
