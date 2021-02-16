b1bca76fe33b220ea6524e145a32c7f32319c4fa
Maryna Makukhina <maryna.makukhina@gmail.com>
2021-02-16 17:06:37 +0200

    update service page structure, add tags

--- src/components/tags/Tags.js
+++ src/components/tags/Tags.js
@@ -2,16 +2,25 @@ import React from "react";
 
 import styles from "./Tags.module.css";
 
-const Tags = ({tags}) => {
+const Tags = ({ tags, selectedTagsIds }) => {
+    let selectedTagsNames = tags.reduce((accumulator, currentValue) => {
+        if (selectedTagsIds.find(item => item === currentValue.id)) {
+            accumulator.push(currentValue.title);
+        }
+        return accumulator;
+    }, []);
+
     return (
         <div className={styles.wrapper}>
             <div>TAGS</div>
             <div className={styles.smallDivider}>....</div>
-            {
-                tags?.map(tag => <span className={styles.tag} key={tag}>{tag};</span>)
-            }
+            {selectedTagsNames?.map(tag => (
+                <span className={styles.tag} key={tag}>
+                    {tag};
+                </span>
+            ))}
         </div>
-    )
+    );
 };
 
 export default Tags;
--- src/pages/services.js
+++ src/pages/services.js
@@ -1,17 +1,26 @@
-import React from "react"
-
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import React, { useState, useEffect } from "react";
+import Layout from "../components/layout";
+import SEO from "../components/seo";
 import ServicesBlock from "../components/services-block/ServicesBlock";
+import TagsService from "../api/Tags.service";
 
-const ServicesPage = () => (
-    <Layout>
-        <SEO title="Services Page" />
+const ServicesPage = () => {
+    const [tags, setTagsData] = useState(null);
 
-        <div className='container'>
-            <ServicesBlock />
-        </div>
-    </Layout>
-);
+    useEffect(() => {
+        TagsService.getTags().then(result => {
+            setTagsData(result);
+        });
+    }, []);
 
+    return (
+        <Layout>
+            <SEO title="Services Page" />
+
+            <div className="container">
+                <ServicesBlock tags={tags} />
+            </div>
+        </Layout>
+    );
+};
 export default ServicesPage;
